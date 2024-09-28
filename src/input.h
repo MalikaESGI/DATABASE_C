3d32aa0d9a13a852785e04c5a3a3726f214901b6
MalikaESGI <malikamechkal@gmail.com>
2024-09-28 16:20:05 +0200

    [INPUT BUFFER]

---
+++ src/input.h
@@ -0,0 +1,18 @@
+#ifndef INPUT_H
+#define INPUT_H
+
+#include <unistd.h>
+
+// Déclaration de la structure InputBuffer
+typedef struct {
+    char* buffer;
+    size_t buffer_length;
+    ssize_t input_length;
+} InputBuffer;
+
+// Prototypes des fonctions
+InputBuffer* new_input_buffer();
+void read_input(InputBuffer* input_buffer);
+void close_input_buffer(InputBuffer* input_buffer);
+
+#endif // INPUT_H
